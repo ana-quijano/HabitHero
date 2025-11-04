38f243a13aca1a2e946bbba77eac22d71086ac95
ana-quijano <anakarinainez@gmail.com>
2025-11-03 20:01:49 -0500

    Switch to live db

--- HabitHero.Api/Program.cs
+++ HabitHero.Api/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddCors(options =>
 
 //var db = "dbServerConnection";
 //var local = "DefaultConnection";        // LOCAL
-var local = "dbServerConnection";     // SEVER
+var local = "dbServerConnection";     // SERVER
 
 var conn = builder.Configuration.GetConnectionString(local); // Set in secrets.json
 builder.Services.AddDbContext<HabitHeroDbContext>(o => o.UseSqlServer(conn));
